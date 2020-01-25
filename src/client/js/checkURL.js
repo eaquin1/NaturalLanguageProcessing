90bbed2cb5f9bae38576beb22c1b295b09b75e72
Emily Aquin <eaquin1@gmail.com>
2020-01-24 21:37:46 -0600

    removed extra alert

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,10 +1,9 @@
 function checkURL(inputText) {
-  var regex = RegExp('^(http|https):\/\/');
+  let regex = RegExp('^(http|https):\/\/');
   if (regex.test(inputText) == false) {
       alert('The url is not valid! It needs to start with \"http(s)://\".');
       return false;
   } else {
-      alert('The url is valid.');
       return true;
   }
 }
