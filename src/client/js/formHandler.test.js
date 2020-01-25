2592b6955e19abda6253088fd76caeee70a5727e
Emily Aquin <eaquin1@gmail.com>
2020-01-24 21:26:23 -0600

    refactored testing

--- src/client/js/formHandler.test.js
+++ src/client/js/formHandler.test.js
@@ -1,4 +1,10 @@
 import { handleSubmit } from './formHandler'
+import { checkURL } from './checkURL'
+
+test('Testing that the data returned is an object', async () => {
+  const data = await checkURL;
+  expect(data).toBeInstanceOf(Object)
+})
 
 test('checkApi', () => {
   expect(handleSubmit).toBeInstanceOf(Function)
